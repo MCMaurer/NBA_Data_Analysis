0d6eb3a3ba1538a22d2f531fa899159a6c218dab
Michael Culshaw-Maurer <culshawmaurer@email.arizona.edu>
2021-11-10 10:56:32 -0600

    added flexibility to team_data and fixed some naming issues in player_data

--- src/data_scraping_functions.R
+++ src/data_scraping_functions.R
@@ -35,7 +35,8 @@ get_player_data <- function(year = 2022, totals = T, per_100 = T,
     pluck(1) %>% 
     janitor::clean_names() %>% 
     filter(tm != "TOT",
-           player != "Player")
+           player != "Player") %>% 
+    select(-rk)
   
   if(totals){
     player_data <- tot
@@ -57,7 +58,8 @@ get_player_data <- function(year = 2022, totals = T, per_100 = T,
         janitor::clean_names() %>% 
         filter(tm != "TOT",
                player != "Player") %>% 
-        rename_with(.cols = -c(1:8), .fn = ~ paste0(.x, "_p100p"))
+        rename_with(.cols = -c(1:8), .fn = ~ paste0(.x, "_p100p")) %>% 
+        select(-rk)
       
       player_data <- player_data %>% 
         left_join(pos) 
@@ -78,7 +80,8 @@ get_player_data <- function(year = 2022, totals = T, per_100 = T,
         janitor::clean_names() %>% 
         filter(tm != "TOT",
                player != "Player") %>% 
-        select(-starts_with("x"))
+        select(-starts_with("x")) %>% 
+        select(-rk)
       
       player_data <- player_data %>% 
         left_join(adv) 
@@ -109,7 +112,8 @@ get_player_data <- function(year = 2022, totals = T, per_100 = T,
         select(-starts_with("totals")) %>% 
         mutate(across(starts_with("position_"), 
                       .fns = ~ str_remove(.x, "%") %>% 
-                        as.numeric() %>% `/`(100)))
+                        as.numeric() %>% `/`(100))) %>% 
+        select(-rk)
       
       player_data <- player_data %>% 
         left_join(pbp) 
@@ -126,20 +130,24 @@ get_player_data <- function(year = 2022, totals = T, per_100 = T,
         html_nodes('#shooting_stats') %>%
         html_table(header = T) %>%
         pluck(1) %>% 
-        tibble::repair_names() %>% 
-        select(where(~ !all(is.na(.x))))
+        set_tidy_names()
       
-      colnames(shoot)[str_starts(colnames(shoot), "^V")] <- shoot[1,][str_starts(colnames(shoot), "^V")]
-      
-      colnames(shoot)[!(colnames(shoot) == shoot[1,])] <- paste(colnames(shoot)[!(colnames(shoot) == shoot[1,])], shoot[1,][!(colnames(shoot) == shoot[1,])], sep = "_")
-      
-      shoot <- shoot[-1,]
-      
-      shoot <- shoot %>% 
-        janitor::clean_names() %>% 
-        filter(tm != "TOT",
-               player != "Player") %>% 
-        rename(mean_fga_dist = dist)
+        colnames(shoot)[str_starts(colnames(shoot), "\\.\\.")] <- shoot[1,][str_starts(colnames(shoot), "^\\.\\.")]
+        
+        shoot <- shoot %>% 
+          select(!`NA`)
+        
+        colnames(shoot) <- str_remove(colnames(shoot), "\\.\\.[0-9]+")
+        
+        colnames(shoot)[!(colnames(shoot) == shoot[1,])] <- paste(colnames(shoot)[!(colnames(shoot) == shoot[1,])], shoot[1,][!(colnames(shoot) == shoot[1,])], sep = "_")
+        
+        shoot <- shoot[-1,]
+        
+        shoot <- shoot %>% 
+          janitor::clean_names() %>% 
+          filter(tm != "TOT",
+                 player != "Player") %>% 
+          select(-rk)
       
       player_data <- player_data %>% 
         left_join(shoot) 
@@ -180,7 +188,8 @@ get_player_data <- function(year = 2022, totals = T, per_100 = T,
         janitor::clean_names() %>% 
         rename(tm = team) %>% 
         filter(tm != "TOT",
-               player != "Player")
+               player != "Player") %>% 
+        select(-rk)
       
       player_data <- player_data %>% 
         left_join(adj) 
@@ -194,39 +203,157 @@ get_player_data <- function(year = 2022, totals = T, per_100 = T,
       pos %in% c("G", "PG", "SG") ~ "G"
     ),
     year = year) %>% 
-    rename(player_rk = rk) %>% 
     type_convert()
   
   return(player_data)
 }
 
-get_team_ratings <- function(year = 2022){
+get_team_data <- function(year = 2022, totals = T, per_100 = T,
+                          advanced = T, shooting = T, ratings = T){
   
   read_html_s <- possibly(read_html, otherwise = NULL)
+  html_node_s <- possibly(html_node, otherwise = NULL)
   
-  url <- paste0("https://www.basketball-reference.com/leagues/NBA_", year, "_ratings.html")
+  page <- paste0("https://www.basketball-reference.com/leagues/NBA_", 
+                year, ".html") %>% 
+    read_html_s()
 
-  team_ratings <- url %>% 
-    read_html_s %>%
-    html_nodes("#ratings") %>% 
-    html_table() %>% 
-    pluck(1)
+  if(totals){
+    node <- page %>% 
+      html_node_s("#totals-team")
+    
+    if(!is_null(node)){
+      team_data <- node %>% 
+        html_table() %>% 
+        janitor::clean_names() %>% 
+        filter(team != "League Average") %>% 
+        select(-rk) %>% 
+        type_convert()
+    }
   
-  # column names are messed up because they're spread across two rows in the table. Here we combine them
-  colnames(team_ratings)[str_starts(colnames(team_ratings), "^$")] <- team_ratings[1,][str_starts(colnames(team_ratings), "^$")]
+  } else {
+    team_data <- node %>% 
+      html_table() %>% 
+      janitor::clean_names() %>% 
+      filter(team != "League Average") %>% 
+      select(team)
+  }
   
-  colnames(team_ratings)[str_detect(colnames(team_ratings), "justed")] <- colnames(team_ratings)[str_detect(colnames(team_ratings), "justed")] %>% 
-    str_remove_all("\\.[0-9]") %>% 
-    paste(team_ratings[1,][str_detect(colnames(team_ratings), "justed")], sep = "_")
+  if(per_100){
+    node <- page %>% 
+      html_node_s("#per_poss-team")
+    
+    if(!is_null(node) & !(class(node) == "xml_missing")){
+      per <- node %>% 
+        html_table() %>% 
+        janitor::clean_names() %>% 
+        filter(team != "League Average") %>% 
+        select(-rk) %>% 
+        rename_with(.cols = -c(1:3), .fn = ~ paste0(.x, "_p100p")) %>% 
+        type_convert()
+      
+      team_data <- team_data %>% 
+        left_join(per)
+    }
+  }
   
-  team_ratings <- team_ratings[-1,]
+  if(advanced){
+    node <- page %>% 
+      html_node_s("#advanced-team")
+    
+    if(!is_null(node) & !(class(node) == "xml_missing")){
+      adv <- node %>% 
+        html_table() %>% 
+        set_tidy_names()
+      
+      colnames(adv)[str_starts(colnames(adv), "\\.\\.")] <- adv[1,][str_starts(colnames(adv), "^\\.\\.")]
+      
+      adv <- adv %>% 
+        select(!`NA`)
+      
+      colnames(adv) <- str_remove(colnames(adv), "\\.\\.[0-9]+")
+      
+      colnames(adv)[!(colnames(adv) == adv[1,])] <- paste(colnames(adv)[!(colnames(adv) == adv[1,])], adv[1,][!(colnames(adv) == adv[1,])], sep = "_")
+      
+      adv <- adv[-1,]
+      
+      adv <- adv %>% 
+        janitor::clean_names() %>% 
+        filter(team != "League Average") %>% 
+        rename(mean_age = age) %>% 
+        select(-rk) %>% 
+        type_convert()
+      
+      team_data <- team_data %>% 
+        left_join(adv)
+    }
+  }
   
-  team_ratings <- team_ratings %>% 
-    mutate(year = year) %>% 
-    janitor::clean_names() %>% 
-    type_convert()
+  if(shooting){
+    node <- page %>% 
+      html_node_s("#shooting-team")
+    
+    if(!is_null(node) & !(class(node) == "xml_missing")){
+      shoot <- node %>% 
+        html_table() %>% 
+        set_tidy_names()
+      
+      colnames(shoot)[str_starts(colnames(shoot), "\\.\\.")] <- shoot[1,][str_starts(colnames(shoot), "^\\.\\.")]
+      
+      shoot <- shoot %>% 
+        select(!`NA`)
+      
+      colnames(shoot) <- str_remove(colnames(shoot), "\\.\\.[0-9]+")
+      
+      colnames(shoot)[!(colnames(shoot) == shoot[1,])] <- paste(colnames(shoot)[!(colnames(shoot) == shoot[1,])], shoot[1,][!(colnames(shoot) == shoot[1,])], sep = "_")
+      
+      shoot <- shoot[-1,]
+      
+      shoot <- shoot %>% 
+        janitor::clean_names() %>% 
+        filter(team != "League Average") %>% 
+        rename(mean_dist = dist) %>% 
+        select(-rk) %>% 
+        type_convert()
+      
+      team_data <- team_data %>% 
+        left_join(shoot)
+    }
+    
+  }
   
-  return(team_ratings)
+  if(ratings){
+    url <- paste0("https://www.basketball-reference.com/leagues/NBA_", year, "_ratings.html")
+    
+    team_ratings <- url %>% 
+      read_html_s %>%
+      html_nodes("#ratings") %>% 
+      html_table() %>% 
+      pluck(1)
+    
+    # column names are messed up because they're spread across two rows in the table. Here we combine them
+    colnames(team_ratings)[str_starts(colnames(team_ratings), "^$")] <- team_ratings[1,][str_starts(colnames(team_ratings), "^$")]
+    
+    colnames(team_ratings)[str_detect(colnames(team_ratings), "justed")] <- colnames(team_ratings)[str_detect(colnames(team_ratings), "justed")] %>% 
+      str_remove_all("\\.[0-9]") %>% 
+      paste(team_ratings[1,][str_detect(colnames(team_ratings), "justed")], sep = "_")
+    
+    team_ratings <- team_ratings[-1,]
+    
+    team_ratings <- team_ratings %>% 
+      mutate(year = year) %>% 
+      janitor::clean_names() %>% 
+      type_convert()
+    
+    team_data <- team_data %>% 
+      left_join(team_ratings)
+  }
+    
+    team_data <- team_data %>% 
+      type_convert() %>% 
+      mutate(year = year)
+  
+    return(team_data)
 }
 
 team_colors <- c("#E03A3E", "#008248", "black", "#1D1160", "#CE1141", "#6F2633", "#007DC5", "#5091CD", "#ED174C", "#243E90", "#CE1141", "#002D62", "#ED174C", "#552583", "#00285E", "#98002E", "#00471B", "#005083", "#002B5C", "#F58426", "#007AC1", "#0B77BD", "#006BB6", "#1D1160", "#E13A3E", "#5A2D81", "#C4CED4", "#CD1141", "#0C2340", "#002B5C")
